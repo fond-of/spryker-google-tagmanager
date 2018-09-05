b8bcb8312ffe386f92f9b8d1a24b7fb9f1c5c933
Pascal Fischer <pascal.fischer@fondof.de>
2018-09-05 11:47:53 +0200

    remove tests, new tests coming soon

--- tests/_data/config_default.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use FondOfSpryker\Shared\GoogleTagManager\GoogleTagManagerConstants;
-
-$config[GoogleTagManagerConstants::CONTAINER_ID] = 'GTM-XXXX';
-$config[GoogleTagManagerConstants::ENABLED] = 'true';
