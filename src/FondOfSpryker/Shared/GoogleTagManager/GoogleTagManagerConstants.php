471a7297bf797ecd4e0a590cdd45527d4cff57ba
Jozsef Geng <gengjozsef86@gmail.com>
2019-07-09 10:28:40 +0200

    add missing attributes

--- src/FondOfSpryker/Shared/GoogleTagManager/GoogleTagManagerConstants.php
+++ src/FondOfSpryker/Shared/GoogleTagManager/GoogleTagManagerConstants.php
@@ -1,14 +1,13 @@
 <?php
 
-/**
- * MIT License
- * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
- */
-
 namespace FondOfSpryker\Shared\GoogleTagManager;
 
 interface GoogleTagManagerConstants
 {
     const CONTAINER_ID = '';
     const ENABLED = false;
+
+    const ATTRIBUTE_SPECIAL_PRICE = 'ATTRIBUTE_SPECIAL_PRICE';
+    const ATTRIBUTE_SPECIAL_PRICE_FROM = 'ATTRIBUTE_SPECIAL_PRICE_FROM';
+    const ATTRIBUTE_SPECIAL_PRICE_TO = 'ATTRIBUTE_SPECIAL_PRICE_TO';
 }
