2b37509df6c65a17732cb5b6a1d77f541c99f9a1
Pascal Fischer <p-fischer@gmx.net>
2020-03-09 13:19:56 +0100

    part of generic events changes

--- src/FondOfSpryker/Yves/GoogleTagManager/GoogleTagManagerDependencyProvider.php
+++ src/FondOfSpryker/Yves/GoogleTagManager/GoogleTagManagerDependencyProvider.php
@@ -17,7 +17,6 @@ use FondOfSpryker\Yves\GoogleTagManager\Plugin\EnhancedEcommerce\EnhancedEcommer
 use FondOfSpryker\Yves\GoogleTagManager\Plugin\EnhancedEcommerce\EnhancedEcommerceCheckoutPaymentPlugin;
 use FondOfSpryker\Yves\GoogleTagManager\Plugin\EnhancedEcommerce\EnhancedEcommerceCheckoutSummaryPlugin;
 use FondOfSpryker\Yves\GoogleTagManager\Plugin\EnhancedEcommerce\EnhancedEcommerceProductDetailPlugin;
-use FondOfSpryker\Yves\GoogleTagManager\Plugin\EnhancedEcommerce\EnhancedEcommerceProductImpressions;
 use FondOfSpryker\Yves\GoogleTagManager\Plugin\EnhancedEcommerce\EnhencedEcommercePurchasePlugin;
 use FondOfSpryker\Yves\GoogleTagManager\Plugin\Mapper\EnhancedEcommerceProductMapper\BrandProductFieldMapperPlugin;
 use FondOfSpryker\Yves\GoogleTagManager\Plugin\Mapper\EnhancedEcommerceProductMapper\Dimension10ProductFieldMapperPlugin;
@@ -309,7 +308,6 @@ class GoogleTagManagerDependencyProvider extends AbstractBundleDependencyProvide
             EnhancedEcommerceConstants::PAGE_TYPE_CHECKOUT_PAYMENT => new EnhancedEcommerceCheckoutPaymentPlugin(),
             EnhancedEcommerceConstants::PAGE_TYPE_CHECKOUT_SUMMARY => new EnhancedEcommerceCheckoutSummaryPlugin(),
             EnhancedEcommerceConstants::PAGE_TYPE_PURCHASE => new EnhencedEcommercePurchasePlugin(),
-            EnhancedEcommerceConstants::PAGE_TYPE_CATEGORY => new EnhancedEcommerceProductImpressions(),
         ];
     }
 
